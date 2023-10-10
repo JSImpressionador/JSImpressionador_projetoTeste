685e0e4d1a6d322d736736230c54960d66899776
Daniel Porto <danieldsporto@outlook.com>
2023-10-10 14:53:03 -0300

    contribuiçao de Daniel, O Dev

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-import { turboLogger } from "./turboLogger.js";
+import { turboLogger } from './turboLogger.js';
 
-console.log("Estou usando o github");
-turboLogger("estou aprendendo o fluxo do git", "red");
+console.log('Estou usando o github');
+turboLogger('estou aprendendo o fluxo do git', 'red');
+turboLogger('Daniel, o Dev, deixou sua contribuição nesse projeto!', 'white');
